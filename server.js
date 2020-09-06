49d63682a684b39425b702242c1e04355751486e
Albrecht1890 <albrecht.bernstorff@web.de>
2020-09-06 18:14:22 +0200

    add mongoDB database, data folder and colors package

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Rout files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // LOAD env vars
 dotenv.config({ path: './config/config.env' });
+
+// connnect to DB
+connectDB();
+
+// Rout files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,7 +25,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold
+  )
 );
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close app & exit process
+  server.close(() => process.exit(1));
+});
