6bada7b56881cd4028926c808c67ac15d005900f
Albrecht1890 <albrecht.bernstorff@web.de>
2020-09-06 10:38:41 +0200

    add basic routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Rout files
+const bootcamps = require('./routes/bootcamps');
+
 // LOAD env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
