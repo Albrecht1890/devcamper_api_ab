9002ec55d3142bdd2570429e99cfa04bd8105bb6
Albrecht1890 <albrecht.bernstorff@web.de>
2020-10-28 18:35:24 +0100

    handling ValidationError and Duplicate key error

--- middleware/error.js
+++ middleware/error.js
@@ -5,13 +5,25 @@ function errorHandler(err, req, res, next) {
 
   error.message = err.message;
 
-  // Log to console for the developer
-  console.log(err.stack.red);
+  // Log to console for the dev
+  console.log(err);
 
   // Mongoose bad ObjectId
-  if (err.name == 'CastError') {
+  if (err.name === 'CastError') {
     const message = `Resource not found with id ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose ValidationError
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
