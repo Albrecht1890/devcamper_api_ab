af55491280149f0a7268184cc64fef9caeab3a8f
Albrecht1890 <albrecht.bernstorff@web.de>
2020-10-30 10:16:23 +0100

    add populate, virtuals and cascade delete to bootcamps/courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -19,7 +19,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
   // finding resource 
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
   // select fields
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
@@ -122,13 +122,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
 
   res.status(200).json({
     success: true,
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,6 +99,9 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
 });
 
 // create bootcamp slug from the name
@@ -125,5 +128,20 @@ BootcampSchema.pre('save', async function (next) {
   this.address = undefined;
   next();
 });
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id} - ${this.name}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+})
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
