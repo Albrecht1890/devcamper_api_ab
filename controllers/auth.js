945dfe5fb53b53695d5d095813245692f929f422
Albrecht1890 <albrecht.bernstorff@web.de>
2020-10-30 15:55:13 +0100

    add register and login routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,5 +16,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true })
+  // create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
