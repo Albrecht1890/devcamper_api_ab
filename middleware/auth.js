15e805c1cb1b5a09fd5c4976816600c49cf39d85
Albrecht1890 <albrecht.bernstorff@web.de>
2020-11-03 11:36:46 +0100

    created admin user cruds

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,7 +17,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   // make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -27,7 +27,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 })
 
