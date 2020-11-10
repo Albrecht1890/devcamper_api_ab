bcb23cd6e174c3ec30afe170fd8943e629cced29
Albrecht1890 <albrecht.bernstorff@web.de>
2020-11-10 07:43:20 +0100

    add sanitizer to prevent nosql injections

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const mongoSanitize = require('express-mongo-sanitize');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
@@ -36,6 +37,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileUpload());
+
+// Sanitize Data / prevent NoSQL injection
+app.use(mongoSanitize());
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
